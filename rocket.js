b89c7b9e073babade81a004fa6ee82086031dc9d
juuliasofia <julia.tuominen.24@edu.luksia.fi>
2025-05-08 00:23:28 +0300

    modified

--- rocket.js
+++ rocket.js
@@ -8,13 +8,29 @@ var changeState = function (state) {
     document.getElementById("countdown").
     innerHTML = countdownNumber;
 
+    //countdown
     if (state == 2) {
         timer = setInterval(function () { countdownNumber = countdownNumber -1;
             document.getElementById("countdown").innerHTML =
             countdownNumber;
+
+            if (countdownNumber > 4 && countdownNumber <= 7) {
+                // be nervous
+                document.getElementById("nervous").className = "nervous show"
+            } else {  document.getElementById("nervous").className = "nervous"
+
+            }
+
+            if (countdownNumber > 1 && countdownNumber <= 4) {
+                // cant wait
+                document.getElementById("cant-wait").className = "cant-wait show"
+            } else { document.getElementById("cant-wait").className = "cant-wait"
+
+            }
+
             if (countdownNumber <= 0) {
                 changeState(3);
-            }
+            };
         }, 500);
     } else if (state == 3) {
         var success = setTimeout(function ()
@@ -25,7 +41,7 @@ var changeState = function (state) {
                     console.log("randomNumber:", randomNumber)
 
                         //success
-                    if (randomNumber > 5) {
+                    if (randomNumber > 2) {
                         changeState(4);
         
                     } else {
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+setTimeout(function () {
+    location.href = "rocket.html";
+}, 2000);
+
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById("cool").className;
+
+    if (currentClassName == "cool") {
+        document.getElementById("cool").className = "cool red";
+    } else {
+        document.getElementById("cool").className = "cool";
+    }
+}
 var car = {
     make: "VW",
     type: "Polo",
